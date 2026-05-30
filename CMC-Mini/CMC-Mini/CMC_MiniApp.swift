f8e41c0291d5730b73b80e6830660d64fcba6e53
Jumy <kdjun97@gmail.com>
2026-05-30 13:46:53 +0900

    init project

---
+++ CMC-Mini/CMC-Mini/CMC_MiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  CMC_MiniApp.swift
+//  CMC-Mini
+//
+//  Created by 김동준 on 5/30/26.
+    
+
+import SwiftUI
+
+@main
+struct CMC_MiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
